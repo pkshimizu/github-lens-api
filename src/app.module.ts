782d7adfed5794fb6d6040dcc6266546e9292c0c
shimizu kenji <shimizu.kenji@noncore.net>
2022-11-02 22:11:16 +0900

    feat: DBの接続設定を追加

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import {TypeOrmModule} from "@nestjs/typeorm";
+import {ConfigModule} from "@nestjs/config";
+
+const config = ConfigModule.forRoot({
+  envFilePath: ['.env.local']
+})
+
+const db = TypeOrmModule.forRoot({
+  type: "mysql",
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  synchronize: false
+})
 
 @Module({
-  imports: [],
+  imports: [config, db],
   controllers: [AppController],
   providers: [AppService],
 })
