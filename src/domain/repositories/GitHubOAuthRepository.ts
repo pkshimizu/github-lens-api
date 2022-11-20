c11ac86f1e2d1f396790af16374d35bf50646023
shimizu kenji <shimizu.kenji@noncore.net>
2022-11-20 21:28:40 +0900

    feat: rebuild project

--- src/domain/repositories/GitHubOAuthRepository.ts
+++
@@ -1,6 +0,0 @@
-import { GitHubUser } from '../models';
-
-export interface GitHubOAuthRepository {
-  getAccessToken(code: string): Promise<string>;
-  getUser(accessToken: string): Promise<GitHubUser>;
-}
--- src/domain/repositories/UserRepository.ts
+++
@@ -1,6 +0,0 @@
-import { GitHubUser, User } from '../models';
-
-export interface UserRepository {
-  saveUser(gitHubUser: GitHubUser): Promise<User>;
-  findUserByUid(uid: string): Promise<User>;
-}
